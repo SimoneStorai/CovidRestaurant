7615cd840a2641347d2e787254276700d1a5dcc4
SimoneStorai <51245581+SimoneStorai@users.noreply.github.com>
2020-06-09 01:15:33 +0200

    Initial getDishes API method implementation.

---
+++ api/getDishes.php
@@ -0,0 +1,27 @@
+<?php
+    include("../util/integer.php");
+    include("../util/string.php");
+    include("../config/db.php");
+
+    // Decode authorization credentials.
+    if (!assert_string_array($_POST["mail"], false))
+    {
+        echo("Invalid mail.");
+        return;
+    }
+    $mail = $_POST["mail"];
+
+    if (!assert_string_array($_POST["password"], false))
+    {
+        echo("Invalid password");
+        return;
+    }
+    $password = $_POST["password"];
+
+    // Prepare dishes select statement.
+    $dishes_stmt = $db->prepare("SELECT * FROM `dish`");
+
+    // Parse result.
+    $result = $dishes_stmt->query();
+    echo($result);
+?>
